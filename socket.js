c9d0e5da81fd754ebed9b322bad701c14a935f8b
Mehdi ESSALEHI <messalehi2001@outlook.com>
2021-08-26 02:48:54 +0100

    Added untracked socket file

---
+++ socket.js
@@ -0,0 +1,80 @@
+const { Server } = require('socket.io')
+const jwt = require('jsonwebtoken')
+const Message = require('./models/message')
+const User = require('./models/user')
+
+const start = server => {
+    const io = new Server(server)
+    io.use((socket, next) => {
+        const token = socket.handshake.auth.token || ""
+        console.log(token)
+        if (!token) {
+            return next(new Error("no token"))
+        }
+        try {
+            const user = jwt.verify(token, process.env.JWT_TOKEN)
+            socket.user = user
+            return next()
+        } catch (error) {
+            console.error(error)
+            return next(new Error("invalid token"))
+        }
+    })
+    
+    io.on('connection', async socket => {
+        console.log(`@${socket.user.username} is connected`)
+        // Update user's online status
+        await User.findOneAndUpdate(
+            { userName: socket.user.username },
+            { onlineStatus: true }
+        )
+        // Every connected user joins a room with its username for incoming messages
+        socket.join(socket.user.username)
+        // Emit message that user became online
+        socket.to(socket.user.username).emit('user online', socket.user.username)
+        // Catch incoming messages, save them into DB and sending them to recipient room
+        socket.on('message', async message => {
+            if (socket.user.username === message.from) {
+                try {
+                    await Message.create({ ...message })
+                    socket.to(message.to).emit('message', message)
+                } catch (error) {
+                    console.error(error)
+                }    
+            }
+        })
+        // Catch conversation select & return online status of recipient
+        socket.on('conversation select', async (username, callback) => {
+            console.log(`@${socket.user.username} wants to talk with @${username}`)
+            // Leave old conversation room
+            if (socket.user.conversation && socket.rooms.has(socket.user.conversation)) {
+                socket.leave(socket.user.conversation)
+            }
+            // Join new conversation room
+            socket.join(username)
+            socket.user.conversation = username
+            if (callback) {
+                const { onlineStatus } = await User.findOne({ userName: username }, { onlineStatus: 1, _id: -1 })
+                callback({
+                    onlineStatus: Boolean(onlineStatus)
+                })
+            }
+        })
+        socket.on('connnect_error', error => {
+            if (error instanceof Error) {
+                console.error(error)
+            }
+        })
+        socket.on('disconnect', async () => {
+            socket.to(socket.user.username).emit('user offline', socket.user.username)
+            // update user's online status
+            await User.findOneAndUpdate(
+                { userName: socket.user.username },
+                { onlineStatus: false },
+            )
+            console.log(`@${socket.user.username} disconnected !`)
+        })
+    })    
+}
+
+exports.start = start
